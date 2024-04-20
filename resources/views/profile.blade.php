7bc57b25504c9c5f7aa25f9b639e3ca8e28285aa
Mihail <miha_alekseev_2003@bk.ru>
2024-04-20 17:57:20 +0300

    php-scr_test

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -118,6 +118,13 @@ body {
            <h1>{{$user = auth()->user()->name;}}</h1>
            <p>57/100 Рейтинг</p>
            <a href="#">Контакты</a>
+           <?php
+               $url = "https://favqs.com/api/qotd";
+               $ch = curl_init();
+               $curl_setops($ch)
+
+           ?>
+           <h2>Цитата дня - {{}}</h1>
        </div>
        <div class="content">
            <div class="left-column">
